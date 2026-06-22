3781190f29dec78cc354f94e1aa6de2730cdb343
Thushanya <thushanyajeyathasan@gmail.com>
2026-06-22 13:26:28 +0530

    Proper nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         onClick={closeMenu}
-        className="fixed inset-0 bg-black/60 backdrop-blur-md z-40"
+        className="fixed inset-0 bg-black/60 backdrop-blur-sm z-40"
       />
 
       {/* CENTERED GLASS MENU */}
